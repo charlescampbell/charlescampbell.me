4dac291ac7eddaf2725e523d9a06224f8b536b36
charlescampbell <charlie.campbell14@googlemail.com>
2020-03-19 15:55:19 +0000

    feat(footer): Active links in footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def active_link?(page)
+    return 'active' if request.original_url.to_s.include?(page)
+
+    ''
+  end
+
+  def active_home?
+    return 'active' if request.original_url.to_s == root_url
+
+    ''
+  end
 end
