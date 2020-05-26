c5e7c9661adf0de96c1a223c1ffa795080e2e735
charlescampbell <charlie.campbell14@googlemail.com>
2020-05-26 15:06:01 +0100

    chore(version): Replace package.json with plain version file

--- .versionrc.js
+++ .versionrc.js
@@ -1,5 +1,12 @@
 module.exports = config = {};
 
+config.bumpFiles = [
+  {
+    filename: "VERSION",
+    type: "plain-text"
+  }
+];
+
 const host = "https://github.com/charlescampbell";
 
 config.commitUrlFormat = `${host}/charlescampbell.me/commit/{{hash}}`;
