dca6dc6f7dc64b8430a99223d4a65c5dafd3f902
charlescampbell <charlie.campbell14@googlemail.com>
2020-05-26 12:34:01 +0100

    chore(domain): Update titles to match new domain name

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@
 lock '~> 3.12.1'
 
 set :application, 'campbell-me'
-set :repo_url, 'git@github.com:charlescampbell/campbell.me.git'
+set :repo_url, 'git@github.com:charlescampbell/charlescampbell.me.git'
 set :deploy_to, "/home/deploy/#{fetch :application}"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
        'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
